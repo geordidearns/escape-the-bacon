a7122299a641e04b8416b73e67f7e6614f5a6c5f
Geordi Dearns <geordidearns@gmail.com>
2016-11-07 22:10:42 +1300

    Add game scene to document

--- src/index.js
+++ src/index.js
@@ -13,3 +13,10 @@ const Container            = PIXI.Container,
 // Initialise the variables that may be used & re-assigned throughout the program
 let cat, dungeon, door, burger, message, bacons, healthBar,
     outerBar, innerBar, gameScene, gameOverScene;
+
+// Create the game scene & set the scene to the desired size
+let gameStage   = new Container(),
+    renderer    = autoDetectRenderer(512, 512);
+
+// Append the rendering context to the document (browser window)
+document.body.appendChild(renderer.view);
