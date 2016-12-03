dae8fa9e5ef2186f9376a06ebb229a18243d3312
Geordi Dearns <geordidearns@Geordis-MacBook-Pro-2.local>
2016-12-03 10:31:22 +0100

    Move pig using keyboard movement

--- bundle.js
+++ bundle.js
@@ -73,7 +73,8 @@
 	    outerBar = void 0,
 	    innerBar = void 0,
 	    gameScene = void 0,
-	    gameOverScene = void 0;
+	    gameOverScene = void 0,
+	    state = void 0;
 
 	// Create the game scene & set the scene to the desired size
 	var gameStage = new Container(),
@@ -88,81 +89,147 @@
 	// Setup function - will be used to set up sprites and game items such as keyboard events
 	function setup() {
 
-	    // Create Game scene where the assets will be added to
-	    gameScene = new Container();
-	    gameStage.addChild(gameScene);
+	  // Create Game scene where the assets will be added to
+	  gameScene = new Container();
+	  gameStage.addChild(gameScene);
 
-	    // Add gameboard to Scene
-	    gameBoard = new Sprite(Loader.resources["images/game-board.png"].texture);
-	    gameScene.addChild(gameBoard);
+	  // Add gameboard to Scene
+	  gameBoard = new Sprite(Loader.resources["images/game-board.png"].texture);
+	  gameScene.addChild(gameBoard);
 
-	    // Add the Pig to the Scene
-	    pig = new Sprite(Loader.resources["images/pig.png"].texture);
-	    // Place the pig on the X-Axis
-	    pig.x = 68;
-	    // Place the pig halfway down the gameScene
-	    pig.y = gameScene.height / 2 - pig.height / 2;
-	    // Add the pig to the scene
-	    gameScene.addChild(pig);
+	  // Add the Pig to the Scene
+	  pig = new Sprite(Loader.resources["images/pig.png"].texture);
+	  // Place the pig on the X-Axis
+	  pig.x = 68;
+	  // Place the pig halfway down the gameScene
+	  pig.y = gameScene.height / 2 - pig.height / 2;
+	  // Set the X-axis velocity to zero (not moving)
+	  pig.vx = 0;
+	  // Set the Y-axis velocity to zero (not moving)
+	  pig.vy = 0;
+	  // Add the pig to the scene
+	  gameScene.addChild(pig);
 
-	    // Add the Hay to the Scene
-	    hay = new Sprite(Loader.resources["images/hay.png"].texture);
-	    // Place the hay on the X-Axis
-	    hay.x = gameScene.width - hay.width - 48;
-	    // Place the hay halfway down the gameScene
-	    hay.y = gameScene.height / 2 - hay.height / 2;
-	    // Add the hay to the scene
-	    gameScene.addChild(hay);
+	  // Add the Hay to the Scene
+	  hay = new Sprite(Loader.resources["images/hay.png"].texture);
+	  // Place the hay on the X-Axis
+	  hay.x = gameScene.width - hay.width - 48;
+	  // Place the hay halfway down the gameScene
+	  hay.y = gameScene.height / 2 - hay.height / 2;
+	  // Add the hay to the scene
+	  gameScene.addChild(hay);
 
-	    // Add the Barn to the Scene
-	    barn = new Sprite(Loader.resources["images/barn.png"].texture);
-	    // Place the barn on the X-Axis, not the Y-axis
-	    barn.position.set(10, 0);
-	    // Add the barn to the scene
-	    gameScene.addChild(barn);
+	  // Add the Barn to the Scene
+	  barn = new Sprite(Loader.resources["images/barn.png"].texture);
+	  // Place the barn on the X-Axis, not the Y-axis
+	  barn.position.set(10, 0);
+	  // Add the barn to the scene
+	  gameScene.addChild(barn);
 
-	    // Bacon Pieces
-	    var numberOfBacons = 6,
-	        spacingBetweenBacons = 48,
-	        xOffset = 150,
-	        speedOfMovement = 2,
-	        directionOfMovement = 1;
+	  // Bacon Pieces attributes
+	  var numberOfBacons = 6,
+	      spacingBetweenBacons = 48,
+	      xOffset = 150,
+	      speedOfMovement = 2,
+	      directionOfMovement = 1;
 
-	    bacons = [];
+	  bacons = [];
 
-	    for (var i = 0; i < numberOfBacons; i++) {
+	  for (var i = 0; i < numberOfBacons; i++) {
 
-	        var bacon = new Sprite(Loader.resources["images/bacon.png"].texture);
+	    var bacon = new Sprite(Loader.resources["images/bacon.png"].texture);
 
-	        // Space the bacon using the value above, times the number of bacons there are
-	        var x = spacingBetweenBacons * i + xOffset;
-	        var y = gameHelpers.randomInt(0, gameStage.height - bacon.height);
+	    // Space the bacon using the value above, times the number of bacons there are
+	    var x = spacingBetweenBacons * i + xOffset;
+	    var y = gameHelpers.randomInt(0, gameStage.height - bacon.height);
 
-	        // Assign the bacons x & y position to the x & y variables
-	        bacon.x = x;
-	        bacon.y = y;
+	    // Assign the bacons x & y position to the x & y variables
+	    bacon.x = x;
+	    bacon.y = y;
 
-	        // Setting the Bacons vertical velocity: direction being 1 (down) or -1 (up)
-	        bacon.vy = speedOfMovement * directionOfMovement;
+	    // Setting the Bacons vertical velocity: direction being 1 (down) or -1 (up)
+	    bacon.vy = speedOfMovement * directionOfMovement;
 
-	        // Reverse direction for next Bacon piece
-	        directionOfMovement *= -1;
+	    // Reverse direction for next Bacon piece
+	    directionOfMovement *= -1;
 
-	        // Push each bacon piece into the bacon array
-	        bacons.push(bacon);
+	    // Push each bacon piece into the bacon array
+	    bacons.push(bacon);
 
-	        // Add all the bacons to the game scene
-	        gameScene.addChild(bacon);
-	    }
+	    // Add all the bacons to the game scene
+	    gameScene.addChild(bacon);
+	  }
 
-	    renderer.render(gameStage);
+	  // Pig Movement
+
+	  //Capture the keyboard arrow keys
+	  var left = gameHelpers.keyboard(37),
+	      up = gameHelpers.keyboard(38),
+	      right = gameHelpers.keyboard(39),
+	      down = gameHelpers.keyboard(40);
+
+	  console.log(gameHelpers.keyboard());
+
+	  // On key press - update pig's x & y-axis movements according to the direction needed to go. Natural 
+	  // direction is down, and right (positive values), up and left are negative values. These values represent
+	  // the pixels to move the Pig.
+
+	  // On key release - Check if the opposite key is not pressed, and that the pig is not moving along the
+	  // Opposite Axis
+
+
+	  left.press = function () {
+	    pig.vx = -5, pig.vy = 0;
+	  };
+	  left.release = function () {
+	    pig.vx = 0;if (!right.isDown && pig.vy === 0) ;
+	  };
+
+	  right.press = function () {
+	    pig.vx = 5, pig.vy = 0;
+	  };
+	  right.release = function () {
+	    pig.vx = 0;if (!left.isDown && pig.vy === 0) ;
+	  };
+
+	  up.press = function () {
+	    pig.vx = 0, pig.vy = -5;
+	  };
+	  up.release = function () {
+	    pig.vy = 0;if (!down.isDown && pig.vy === 0) ;
+	  };
+
+	  down.press = function () {
+	    pig.vx = 0, pig.vy = 5;
+	  };
+	  down.release = function () {
+	    pig.vy = 0;if (!up.isDown && pig.vy === 0) ;
+	  };
+
+	  //Set the game state
+	  state = play;
+
+	  gameLoop();
 	};
 
 	// Game Loop function - This is what will be called recursively with the requestAnimationFrame method
-	function gameLoop() {};
+	function gameLoop() {
+
+	  //Loop this function 60 times per second
+	  requestAnimationFrame(gameLoop);
+
+	  state();
+
+	  //Render the stage
+	  renderer.render(gameStage);
+	};
 
 	// Play Function - Where the game logic lives, setting the state of the game if ended or not
-	function play() {};
+	function play() {
+
+	  pig.x += pig.vx;
+	  pig.y += pig.vy;
+	};
 
 	// End Function - what happens when the game has ended?
 	function end() {};
@@ -177,11 +244,49 @@
 	  value: true
 	});
 	exports.randomInt = randomInt;
+	exports.keyboard = keyboard;
 	// Helper functions to assist gameplay. Collision detection, Keyboard events, useful Math functions etc
 
 	// Returns a random integer
 	function randomInt(min, max) {
 	  return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	// Keyboard movement
+	function keyboard(keyCode) {
+	  var key = {};
+	  key.code = keyCode;
+	  key.isDown = false;
+	  key.isUp = true;
+	  key.press = undefined;
+	  key.release = undefined;
+
+	  // The keyDown handler
+	  key.downHandler = function (event) {
+	    if (event.keyCode === key.code) {
+	      if (key.isUp && key.press) key.press();
+	      key.isDown = true;
+	      key.isUp = false;
+	    }
+	    event.preventDefault();
+	  };
+
+	  // The keyUp handler
+	  key.upHandler = function (event) {
+	    if (event.keyCode === key.code) {
+	      if (key.isDown && key.release) key.release();
+	      key.isDown = false;
+	      key.isUp = true;
+	    }
+	    event.preventDefault();
+	  };
+
+	  // Attach event listeners to the window to listen to keyboard interaction
+	  window.addEventListener("keyup", key.upHandler.bind(key), false);
+
+	  window.addEventListener("keydown", key.downHandler.bind(key), false);
+
+	  return key;
 	}
 
 /***/ }
--- src/helpers.js
+++ src/helpers.js
@@ -5,3 +5,44 @@ export function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Keyboard movement
+export function keyboard(keyCode) {
+  let key = {};
+  key.code = keyCode;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+
+  // The keyDown handler
+  key.downHandler = (event) => {
+    if (event.keyCode === key.code) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+    }
+    event.preventDefault();
+  }
+
+  // The keyUp handler
+  key.upHandler = (event) => {
+    if (event.keyCode === key.code) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+    }
+    event.preventDefault();
+  }
+
+  // Attach event listeners to the window to listen to keyboard interaction
+  window.addEventListener(
+    "keyup", key.upHandler.bind(key), false
+  );
+
+  window.addEventListener(
+    "keydown", key.downHandler.bind(key), false
+  );
+
+  return key;
+}
+
